33cec8e81a38bd084389ad79778fe4e525d4e252
Halva773 <Selin0106@mail.ru>
2025-03-07 12:04:13 +0300

    Написан главный файл для основных математических операций

---
+++ main.py
@@ -0,0 +1,19 @@
+def sum(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    return a / b
+
+
+if __name__ == '__main__':
+    a, b = 10, 5
+    print(f"Сумма: {sum(a, b)}")
+    print(f"Разность: {sub(a, b)}")
+    print(f"Произведение: {mul(a, b)}")
+    print(f"Частное: {div(a, b)}")
